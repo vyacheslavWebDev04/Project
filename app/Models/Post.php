1bae301c2ecd30b2d209ceec9734ae973db56b5d
Vyacheslav Raspertov <vyacheslav.rasDev@yahoo.com>
2024-11-07 22:16:40 +0200

    best_version_of_app

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,23 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-		use HasFactory;
-		
-		protected $guarded = [];
-		public $timestamps = false;
-
-		public function category()
-		{
-			return $this->belongsTo(Category::class, 'category_id', 'id');
-		}
-
-		public function tags()
-		{
-			return $this->belongsToMany(Tag::class, 'post_tags', 'post_id','tag_id' );
-		}
+    public function category()
+    {
+return $this->hasMany(Category::class);
+    }
 }
